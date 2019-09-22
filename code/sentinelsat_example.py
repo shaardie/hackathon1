a0573b4dd9b26a9945d68b6f5ea3c17f18b7f3fa
Timo Brockmeyer <timo.brockmeyer@gmail.com>
2019-09-22 14:19:04 +0200

    working example

--- code/sentinelsat_example.py
+++ code/sentinelsat_example.py
@@ -2,14 +2,15 @@ from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 
 user = 's5pguest'
 password = 's5pguest'
-url = 'https://scihub.copernicus.eu/dhus'
-search_polygon = '../data/landkreis_osnabrueck.geojson'
+url = 'https://s5phub.copernicus.eu/dhus'
+search_polygon = './data/landkreis_osnabrueck.geojson'
+start_date = '20190101'
+end_date = '20190201'
 
 api = SentinelAPI(user, password, url)
 footprint = geojson_to_wkt(read_geojson(search_polygon))
-products = api.query(footprint,
-                     producttype='SLC',
-                     orbitdirection='ASCENDING')
+products = api.query(area = footprint, 
+                     date=(start_date, end_date))
 
 api.download_all(products)
 
