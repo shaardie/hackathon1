2ec8ebdecd3a28fccf01e84adc757295cffc5080
Timo Brockmeyer <timo.brockmeyer@gmail.com>
2019-09-22 14:43:20 +0200

    using pandas to filter data

--- code/sentinelsat_example.py
+++ code/sentinelsat_example.py
@@ -4,14 +4,27 @@ user = 's5pguest'
 password = 's5pguest'
 url = 'https://s5phub.copernicus.eu/dhus'
 search_polygon = './data/landkreis_osnabrueck.geojson'
-start_date = '20190101'
-end_date = '20190201'
+start_date = '20190910'
+end_date = '20190911'
 
+# query api for available products
 api = SentinelAPI(user, password, url)
 footprint = geojson_to_wkt(read_geojson(search_polygon))
-products = api.query(area = footprint, 
+products = api.query(area=footprint,
                      date=(start_date, end_date))
 
-api.download_all(products)
+# convert to panas data frame
+products_df = api.to_dataframe(products)
+
+# inspect data
+products_df.head()  # view top of df
+products.columns  # show column names
+
+# filter only one product of CO
+where = products_df.producttypedescription == 'Carbon Monoxide'
+one_id = products_df.uuid[where][0]
+
+# download one product
+api.download(one_id)
 
 
